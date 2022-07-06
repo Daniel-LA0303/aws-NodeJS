a5f4b8efb8a647ef4439b3ec3d04d6faa3e62654
Daniel-La0303 <03daniel030399@gmail.com>
2022-07-05 19:10:11 -0500

    modificacion

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world with espress.js'));
+app.get('/', (req, res) => res.send('<h2>esto es un cambio del proyecto de NODEJS</h2>'));
 
 app.listen(3000);
 console.log('server on port 3000');
